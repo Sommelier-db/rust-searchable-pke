7e1ba8ae6ce0bf745ef02155a845d363646072e7
SoraSuegami <suegamisora@gmail.com>
2022-10-06 15:51:11 +0900

    Rename the modules.

--- src/hashes.rs
+++ src/hashes.rs
@@ -1,13 +1,23 @@
-use crate::PEKSError;
 use groupy::CurveProjective;
 use paired::{Compress, Engine, HashToCurve};
 use sha2::{Digest, Sha256};
+use std::io;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ECHashError {
+    #[error("IO error: {}", source)]
+    Io {
+        #[from]
+        source: io::Error,
+    },
+}
 
 pub(crate) fn hash_bytes2point<E: Engine>(bytes: &[u8]) -> E::G1 {
     <E::G1 as CurveProjective>::hash(bytes)
 }
 
-pub(crate) fn hash_field2bytes<E: Engine>(field: E::Fqk) -> Result<Vec<u8>, PEKSError> {
+pub(crate) fn hash_field2bytes<E: Engine>(field: E::Fqk) -> Result<Vec<u8>, ECHashError> {
     let mut field_bytes = Vec::new();
     field.write_compressed(&mut field_bytes)?;
     Ok(Sha256::digest(&field_bytes).to_vec())
