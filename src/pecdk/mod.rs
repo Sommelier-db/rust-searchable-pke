1d13518186b68f1b5bcabc9a07fc8f49f116673d
SoraSuegami <suegamisora@gmail.com>
2022-10-24 03:26:06 +0900

    Fix test_pecdk_size.

--- src/pecdk/mod.rs
+++ src/pecdk/mod.rs
@@ -508,33 +508,32 @@ mod test {
             let keyword = (0..16).map(|_| thread_rng.gen()).collect::<Vec<u8>>();
             keywords.push(keyword);
         }
-        let m = 64;
         let secret_key = SecretKey::<Bls12>::gen(&mut rng, n);
         let sk_json = serde_json::to_string(&secret_key).unwrap();
-        println!("sk_json {}", sk_json.len());
+        //println!("sk_json {}", sk_json.len());
         let public_key = secret_key.into_public_key(&mut rng);
         let pk_json = serde_json::to_string(&public_key).unwrap();
-        println!("pk_json {}", pk_json.len());
+        //println!("pk_json {}", pk_json.len());
         let ct = public_key
             .encrypt::<_, Fr>(keywords.clone(), &mut rng)
             .unwrap();
         let ct_json = serde_json::to_string(&ct).unwrap();
-        println!("ct_json {}", ct_json.len());
+        //println!("ct_json {}", ct_json.len());
         let trapdoor = secret_key
             .gen_trapdoor::<_, Fr>(keywords, SearchSym::AND, &mut rng)
             .unwrap();
         let td_json = serde_json::to_string(&trapdoor).unwrap();
-        println!("td_json {}", td_json.len());
+        //println!("td_json {}", td_json.len());
     }
 }
 
-// n=128
+// n=128 (bytes)
 // sk: 21735 21735 21735 21735 21735 21735 21735 21735 21735 21735
 // pk: 91064 91064 91064 91064 91064 91064 91064 91064 91064 91064
 // ct: 11427364 11427737 11427427 11426151 11427047 11428346 11427486 11428057 11427596
 // td: 44984 45037 44796 45035 45045 44888 44951 45000 44968
 
-// n=64
+// n=64 (bytes)
 // sk: 11086 11086 11086 11086 11086 11086 11086 11086 11086 11086
 // pk: 47112 47112 47112 47112 47112 47112 47112 47112 47112 47112
 // ct: 2893903 2893706 2893835 2894411 2893342 2893720 2893491 2892935 2894045 2894123
